cefeb4b97723247b03525647b4da3ad52a005a68
Renato Girão <renato.sgirao@gmail.com>
2018-08-30 18:23:32 -0300

    correction in users birthday

---
+++ User.swift
@@ -0,0 +1,19 @@
+//
+//  Users.swift
+//  Teste Hanzo
+//
+//  Created by Renato Savoia Girão on 27/08/2018.
+//  Copyright © 2018 Renato Savoia Girão. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    let id : String?
+    let name : String?
+    let image : String?
+    var birthday : String?
+    let bio : String?
+    
+}
